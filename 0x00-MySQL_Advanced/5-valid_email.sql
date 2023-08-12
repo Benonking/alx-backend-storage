7a0967418bb75ad7e57270e093f6a0126269eaf0
Benonking <benonkighoma@gmail.com>
2023-08-12 19:57:02 +0300

    validate email with triggers

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- create trigger that resets  the atrribute "valid_email" 
+-- only if the email has been changed
+DELIMETER //;
+CREATE TRIGGER validate_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+  IF NEW.email != OLD.email THEN
+    SET NEW.valid_email = 0;
+  END IF;
+END 
+/
+DELIMETER ;
