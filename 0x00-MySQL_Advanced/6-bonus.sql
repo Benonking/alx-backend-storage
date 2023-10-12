9b7468203202a7962a3e6aa56927f42f24b8262a
Benonking <benonkighoma@gmail.com>
2023-10-12 14:36:47 +0300

    add stored procedure to add bonus to student

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,14 +1,18 @@
 -- create stored procedure AddBonus
--- Adds new correction for a student
+--    Args:user_id, project_name, score
+-- Adds new correction for a student with given id
 
-CREATE PROCEDURE add_bonus(
+DELIMITER $$ ;
+CREATE PROCEDURE AddBonus(
   IN user_id INTEGER,
   IN project_name VARCHAR(255),
   IN score INTEGER
   )
 BEGIN
   IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
-    INSERT INTO projects (name) VALUES (project_name)
+    INSERT INTO projects (name) VALUES (project_name);
   END IF;
   INSERT INTO corrections (user_id, project_id, score)
-  VALUES (user_id, (SELECT id from projects WHE))
+  VALUES (user_id, (SELECT id from projects WHERE name = project_name), score);
+END $$
+DELIMITER ;
