b3cd1b8ad1c3ea76024eab56c22d47451f42cf93
Benonking <benonkighoma@gmail.com>
2023-10-17 19:53:24 +0300

    insert document in mongo db based on key value pairs

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+Module that defines a function to insert a doc in a mongo db
+'''
+
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+  '''
+  insert doc in collection based on **kwargs
+  Args:
+    mongo_collection
+    **kwargs
+  '''
+  if len(kwargs) <= 0:
+    return None
+  return mongo_collection.insert(kwargs)
