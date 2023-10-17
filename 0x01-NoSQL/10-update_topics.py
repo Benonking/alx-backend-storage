dd223780232f0b8c19173e9e56418ca729776937
Benonking <benonkighoma@gmail.com>
2023-10-17 20:11:12 +0300

    update pymongo collection

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+    Define function to update mongo db
+'''
+import pymongo
+
+def update_topics(mongo_collection, name, topics):
+    '''
+        update mongo database to change all topics of school(doc) based on name
+        Args:
+            name: school name to update
+            topics: list of string to th list of topics approached in school
+            mongo_collection: pymongo collection object
+    '''
+    mongo_collection.update(
+        {"name": name},
+        {"$set":{"topics": topics}}
+        )
