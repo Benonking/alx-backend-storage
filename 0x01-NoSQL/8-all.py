2767c3196dfb5c02cf3d7e106a35cfb2e06a70d6
Benonking <benonkighoma@gmail.com>
2023-10-17 19:33:05 +0300

    print all docs in db usung py mongo

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 '''
 Module 8-all
-script List all documents in a mongo db 
+script List all documents in a mongo db
 '''
 
 import pymongo
 
+
 def list_all(mongo_collection):
-  if not mongo_collection:
-    return []
-  
-  return list(mongo_collection.find())
+    '''
+    function:print all docs in collection
+    Args: collection in db
+    '''
+    if not mongo_collection:
+        return []
+    return list(mongo_collection.find())
