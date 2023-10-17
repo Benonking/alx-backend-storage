f98efaaa341b66f2de0985323b26a98ee93a6639
Benonking <benonkighoma@gmail.com>
2023-10-17 20:43:40 +0300

    search pymongo db collection

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+  Define Function schools_by_topic 
+  Return a list of schools with a specific topic
+'''
+import pymongo
+
+def schools_by_topic(mongo_collection, topic):
+  '''
+    search collectiona and retun documents in mongo db with certain tiopic
+    Args:
+      topic: string to be searched
+      mongo_collection: pymongo collection object
+  '''
+
+  return [topic for topic in mongo_collection.find({"topic": topic})]
