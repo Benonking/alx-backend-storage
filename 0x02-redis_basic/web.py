36794f4b4658c090bb979e286216229d8ab60ddc
Benonking <benonkighoma@gmail.com>
2023-10-21 13:11:12 +0300

    cache html page to redis and set exporation time to 10sec

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,7 +8,7 @@ import redis
 from functools import wraps
 
 # connect to redis server
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_client = redis.Redis()
 
 
 # decorator to cache result in redis
